754956d917960c5f3bf7add4d0ce9e15ea8c427d
KlebersonDuarte <klebersonduartesantos39@gmail.com>
2026-02-16 12:54:23 -0300

    Adicionando gitignore

---
+++ EX33_PRL_261125/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace EX33_PRL_261125
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFigura = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha a opção de imagem";
+            // 
+            // cbFigura
+            // 
+            this.cbFigura.FormattingEnabled = true;
+            this.cbFigura.Items.AddRange(new object[] {
+            "Círculo",
+            "Quadrado",
+            "Elipse",
+            "Pizza",
+            "Retângulo",
+            "Círculo Cheio",
+            "Quadrado Cheio",
+            "Elipse Cheio",
+            "Pizza Cheia",
+            "Retângulo Chieo"});
+            this.cbFigura.Location = new System.Drawing.Point(159, 1);
+            this.cbFigura.Name = "cbFigura";
+            this.cbFigura.Size = new System.Drawing.Size(129, 21);
+            this.cbFigura.TabIndex = 1;
+            this.cbFigura.SelectedIndexChanged += new System.EventHandler(this.cbFigura_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 450);
+            this.Controls.Add(this.cbFigura);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "EX33_PRL_261125";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFigura;
+    }
+}
+
---
+++ EX33_PRL_261125/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX33_PRL_261125
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbFigura_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Graphics MyGraphics = base.CreateGraphics();
+            Pen MyPen = new Pen(Color.DarkRed);
+            SolidBrush MyBrush = new SolidBrush(Color.DarkBlue);
+
+            MyGraphics.Clear(Color.White);
+
+            switch (cbFigura.SelectedIndex) { 
+            case 0:
+                    MyGraphics.DrawEllipse(MyPen, 50, 50, 150, 150);
+                    break;
+                case 1:
+                    MyGraphics.DrawRectangle(MyPen, 50, 50, 150, 150);
+                    break;
+                case 2:
+                    MyGraphics.DrawEllipse(MyPen, 50, 50, 150, 115);
+                    break;
+                case 3:
+                    MyGraphics.DrawPie(MyPen, 50, 50, 150, 150, 0, 45);
+                    break;
+                case 4:
+                    MyGraphics.DrawRectangle(MyPen, 50, 50, 150, 100);
+                    break;
+                case 5:
+                    MyGraphics.FillEllipse(MyBrush, 50, 50, 150, 150);
+                    break;
+                case 6:
+                    MyGraphics.FillRectangle(MyBrush, 50, 50, 150, 100);
+                    break;
+                case 7:
+                    MyGraphics.FillEllipse(MyBrush, 50, 50, 150, 115);
+                    break;
+                case 8:
+                    MyGraphics.FillPie(MyBrush, 50, 50, 150, 150, 0, 45);
+                    break;
+                    case 9:
+                    MyGraphics.FillRectangle(MyBrush, 50, 50, 150, 100);
+                    break;
+               
+            }
+        }
+    }
+}
